7a9b433ac9827ed43aaa369c8a46f39c080e520c
ShreyanshBanerjee <shreyansh.banerjee.x@gmail.com>
2026-07-11 14:17:19 -0400

    very minimal form ui

---
+++ static/script.js
@@ -0,0 +1,6 @@
+function submit() {
+  console.log("user submitted: ");
+  console.log("team: ", document.getElementById("team").value);
+  console.log("budget: ", document.getElementById("budget").value);
+  console.log("date: ", document.getElementById("date").value);
+}
