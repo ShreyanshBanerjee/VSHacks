a9b42f64b83614b812cbd3f7bd2399d55d33faca
ShreyanshBanerjee <shreyansh.banerjee.x@gmail.com>
2026-07-11 17:20:22 -0400

    ui update

--- static/script.js
+++ static/script.js
@@ -3,7 +3,13 @@ function submit() {
   console.log("team: ", document.getElementById("team").value);
   console.log("budget: ", document.getElementById("budget").value);
   console.log("date: ", document.getElementById("date").value);
-  console.log("code: ", document.getElementById("code").value)
+  
+  const button = document.getElementById("submit");
+  button.disabled = true;
+  button.textContent = "Created";
+
+  document.getElementById("code").textContent = "A6xZae";
+  document.getElementById("code_div").style.display = "block";
 }
 
 const teams = [
